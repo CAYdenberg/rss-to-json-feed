e62cda517f50d963bf690a3678bcbde652e05f01
@CAYdenberg <ydenberg@gmail.com>
2018-09-13 20:45:36 -0600

    Parse atom feeds

--- test/parser.js
+++ test/parser.js
@@ -30,7 +30,7 @@ describe('Parser', function() {
   it('should parse Reddit', function(done) {
     testParseForFile('reddit', 'rss', done);
   })
-  
+
   it('should parse sciencemag.org (RSS 1.0)', function(done) {
     testParseForFile('rss-1', 'rss', done);
   })
@@ -39,62 +39,30 @@ describe('Parser', function() {
     testParseForFile('craigslist', 'rss', done);
   })
 
-  // it('should parse atom', function(done) {
-  //   testParseForFile('reddit-atom', 'rss', done);
-  // })
+  it('should parse atom', function(done) {
+    testParseForFile('reddit-atom', 'rss', done);
+  })
 
   it('should parse atom feed', function(done) {
     testParseForFile('gulp-atom', 'atom', done);
   })
 
-  // it('should parse reddits new feed', function(done) {
-  //   testParseForFile('reddit-home', 'rss', done);
-  // })
-  //
-  // it('should parse with missing fields', function(done) {
-  //   testParseForFile('missing-fields', 'atom', done)
-  // })
-  //
-  // it('should parse heise', function(done) {
-  //   testParseForFile('heise', 'atom', done);
-  // })
-  //
-  // it('should parse Instant Article', function(done) {
-  //   testParseForFile('instant-article', 'rss', done);
-  // });
-  //
-  // it('should parse Feedburner', function(done) {
-  //   testParseForFile('feedburner', 'atom', done);
-  // });
-  // it('should parse podcasts', function(done) {
-  //   testParseForFile('narro', 'rss', done);
-  // });
-  //
-  // it('should throw error for unrecognized', function(done) {
-  //   Parser.parseFile(__dirname + '/input/unrecognized.rss', function(err, parsed) {
-  //     Expect(err.message).to.contain('Feed not recognized as RSS');
-  //     done();
-  //   })
-  // })
-  //
-  // it('should parse custom fields', function(done) {
-  //   var options = {
-  //     customFields: {
-  //       feed: ['language', 'copyright', 'nested-field'],
-  //       item: ['subtitle']
-  //     }
-  //   };
-  //   Parser.parseFile(__dirname + '/input/customfields.rss',options, function(err, parsed) {
-  //     Expect(err).to.equal(null);
-  //     var str = JSON.stringify(parsed, null, 2);
-  //     var outfile = OUT_DIR + '/customfields.json';
-  //     if (process.env.WRITE_GOLDEN) {
-  //       FS.writeFileSync(outfile, str);
-  //     } else {
-  //       var expected = FS.readFileSync(outfile, 'utf8');
-  //       Expect(str).to.equal(expected);
-  //     }
-  //     done();
-  //   });
-  // });
+  it('should parse reddits new feed', function(done) {
+    testParseForFile('reddit-home', 'rss', done);
+  })
+
+  it('should parse heise', function(done) {
+    testParseForFile('heise', 'atom', done);
+  })
+
+  it('should parse Feedburner', function(done) {
+    testParseForFile('feedburner', 'atom', done);
+  })
+
+  it('should throw error for unrecognized', function(done) {
+    Parser.parseFile(__dirname + '/input/unrecognized.rss', function(err, parsed) {
+      Expect(err.message).to.contain('Feed not recognized as RSS');
+      done();
+    })
+  })
 })
